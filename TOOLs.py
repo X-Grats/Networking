21ffb1bee2fb3dd64a3cb6191b6f4e4c29a1881e
Tentgrat <114117861+Tentgrat@users.noreply.github.com>
2022-10-05 08:08:34 +0700

    Create TOOLs.py

---
+++ TOOLs.py
@@ -0,0 +1,265 @@
+import os
+
+def menu():
+
+    print(""" 
+.########..#######...#######..##........######...######.
+....##....##.....##.##.....##.##.......##....##.##....##
+....##....##.....##.##.....##.##.......##.......##......
+....##....##.....##.##.....##.##........######...######.
+....##....##.....##.##.....##.##.............##.......##
+....##....##.....##.##.....##.##.......##....##.##....##
+....##.....#######...#######..########..######...######.
+========================================
+Created By Anonymous
+Facebook: facebook.com/kalinuxtutorials
+Ver: 2.0
+----
+ONLY FOR TERMUX!
+----
+==========================================
+00. Turn your Android into Hacking Machine.
+------------------------------------------
+1. Install Nmap 
+2. Install Hydra
+3. Install SQLMap
+4. Install Metasploit
+5. Install ngrok
+6. Install Kali Nethunter
+7. Install angryFuzzer
+8. Install Red_Hawk
+9. Install Weeman
+10. Install IPGeoLocation
+11. Install Cupp
+12. Instagram Bruteforcer (instahack)
+13. Twitter Bruteforcer   (TwitterSniper)
+14. Install Ubuntu
+15. Install Fedora
+16. Install viSQL
+17. Install Hash-Buster
+18. Install D-TECT
+19. Install routersploit
+------------------------------------------
+99. Exit
+==========================================
+""")
+
+loop = True
+
+while loop:
+    menu()
+    what = input("#: ")
+    if what == "00":
+        print("================================")
+        print("This will install: nmap, hydra, sqlmap, metasploit, ngrok, angryFuzzer, red_hawk, weeman, IPGeoLocation, cupp, instahack, TwitterSniper, Hash-Buster, D-TECT, routersploit and viSQL with one click.")
+        print("----------------")
+        hm = input("[?] Do you want to continue? (y/n): ")
+        print("================================")
+        if hm == "y":
+            print("========================================================")
+            print("[+] Please put down you android and go to the toilet...")
+            print("Because this will take a long time.")
+            print("========================================================")
+            os.system("pkg update")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python")
+            os.system("pkg install -y python2")
+            os.system("pkg install -y wget")
+            os.sysetm("pkg install -y nmap")
+            os.system("plg install -y hydra ")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install python2")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg install wget")
+            os.system("cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh")
+            os.system("cd /data/data/com.termux/files/home && bash metasploit.sh")
+            os.system("cd /data/data/com.termux/files/home && cd metasploit-framework")
+            os.system("cd /data/data/com.termux/files/home && gem install bundle --pre")
+            os.system("cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries")
+            os.system("cd /data/data/com.termux/files/home && bundle install")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python2")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git")
+            os.system("cd /data/data/com.termux/files/home && cd angryFuzzer")
+            os.system("cd /data/data/com.termux/files/home && pip2 install -r requirements.txt")
+            os.system("cd /data/data/com.termux/files/home && pip2 install requests")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y php")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Tuhinshubhra/RED_HAWK")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg update -y")
+            os.system("pkg install -y python2")
+            os.system("pkg install -y git")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/evait-security/weeman.git")
+            os.system("cd /data/data/com.termux/files/home && cd weeman")
+            os.system("cd /data/data/com.termux/files/home && chmod +x weeman.py")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/maldevel/IPGeoLocation.git")
+            os.system("cd /data/data/com.termux/files/home && cd IPGeoLocation")
+            os.system("cd /data/data/com.termux/files/home && pip install -r requirements.txt")
+            os.system("cd /data/data/com.termux/files/home")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python")
+            os.system("pkg install -y nano")
+            os.system("pip install requests")
+            os.system("pip install beautifulsoup4")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/avramit/instahack.git")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python")
+            os.system("pip install mechanicalsoup")
+            os.system("pkg install -y nano")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/abdallahelsokary/TwitterSniper.git")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python2")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python2")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/UltimateHackers/Hash-Buster.git")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python2")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/shawarkhanethicalhacker/D-TECT.git")
+            os.system("pkg update -y")
+            os.system("pkg install -y git")
+            os.system("pkg install -y python2")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git")
+            os.system("cd /data/data/com.termux/files/home && cd routersploit")
+            os.system("pip2 install -r requirements.txt")
+            os.system("pip2 install -r requirements-dev.txt")
+            os.system("pip2 install -r requests")
+            os.system("clear")
+            print("========================================")
+            print("[+] everything successfully installed :)")
+            print("[+] Happy Hacking <3")
+            print("========================================")
+        else:
+            rmenu = input("[?] Back to Menu? (y/n): ")
+            if rmenu == "y":
+                menu()
+            else:
+                break
+    if what == "1":
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg update -y")
+        os.system("pkg install -y nmap")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] nmap installed successfully :)")
+        print("[+] Type 'nmap' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "2":
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg update -y")
+        os.system("pkg install -y hydra")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] hydra installed successfully :)")
+        print("[+] Type 'hydra' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "3":
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install python2")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] SQLMap installed successfully :)")
+        print("[+] Go to sqlmap folder and type 'python2 sqlmap.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "4":
+        os.system("pkg install -y wget")
+        os.system("cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh")
+        os.system("cd /data/data/com.termux/files/home && bash metasploit.sh")
+        os.system("cd /data/data/com.termux/files/home && cd metasploit-framework")
+        os.system("cd /data/data/com.termux/files/home && gem install bundle --pre")
+        os.system("cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries")
+        os.system("cd /data/data/com.termux/files/home && bundle install")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] Metasploit installed successfully :)")
+        print("[+] Type 'msfconsole' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "5":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] ngrok installed successfully :)")
+        print("[+] Go to ngrok folder and type './ngrok http 80' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "6":
+        os.system("pkg update -y")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Hax4us/Nethunter-In-Termux.git")
+        os.system("cd /data/data/com.termux/files/home && cd Nethunter-In-Termux && chmod +x kalinethunter")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] Nethunter installed successfully :)")
+        print("[+] Go to Nethunter-In-Termux folder and type './kalinethunter' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "7":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python2")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git")
+        os.system("cd /data/data/com.termux/files/home && cd angryFuzzer")
+        os.system("cd /data/data/com.termux/files/home && pip2 install -r requirements.txt")
+        os.system("cd /data/data/com.termux/files/home && pip2 install requests")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] angryFuzzer installed successfully :)")
+        print("[+] Go to angryFuzzer folder and type 'python2 angryFuzzer.py' to start.")
+        print("====================================")
+        rm
